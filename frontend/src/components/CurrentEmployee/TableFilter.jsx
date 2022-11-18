99822dd7eecf66a657a2b58badc8c608ba01f26d
Rozelier Julien <rozelierjjs@gmail.com>
2022-11-18 15:30:40 +0100

    ajout du labellé searchbar

--- frontend/src/components/CurrentEmployee/TableFilter.jsx
+++ frontend/src/components/CurrentEmployee/TableFilter.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
-import { useAsyncDebounce } from 'react-table';
+import React, { useState } from "react";
+import { useAsyncDebounce } from "react-table";
 
 const TableFilter = ({ filter, setFilter }) => {
-    const [value, setValue] = useState(filter);
-    const onChange = useAsyncDebounce(value => {
-        setFilter(value || undefined)
-    }, 400)
+  const [value, setValue] = useState(filter);
+  const onChange = useAsyncDebounce((value) => {
+    setFilter(value || undefined);
+  }, 400);
 
-    return (
-        <div className='filter-searchBar'>
-            <label htmlFor='searchBar' className='mr-[5px]'>Search</label>
-            <input
-                className='w-[70%] border border-blue-gray-300 rounded-[7px] pl-2 py-[2px] text-blue-gray-700'
-                value={value || ''}
-                onChange={(e) => {
-                    setValue(e.target.value)
-                    onChange(e.target.value)
-                }} />
-        </div>
-    );
+  return (
+    <div className="filter-searchBar">
+      <label htmlFor="searchBar" className="mr-[5px]">
+        Search
+      </label>
+      <input
+        id="searchBar"
+        className="w-[70%] border border-blue-gray-300 rounded-[7px] pl-2 py-[2px] text-blue-gray-700"
+        value={value || ""}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+      />
+    </div>
+  );
 };
 
 export default TableFilter;
